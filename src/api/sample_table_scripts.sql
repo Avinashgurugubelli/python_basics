a8fca98da2b3db49230f9b23c39499fc45627aa4
Avinash Gurugubelli <mylearnings1993@gmail.com>
2022-10-09 23:17:10 +0530

    Create sample_table_scripts.sql

---
+++ src/api/sample_table_scripts.sql
@@ -0,0 +1,11 @@
+CREATE TABLE employees (
+    id int,
+    name varchar(255),
+    email_id varchar(255),
+    phone_number varchar(255)
+);
+
+INSERT INTO `employees` (`id`, `name`, `email_id`, `phone_number`) VALUES ('1', 'Avi', 'avi@xyz.com', '23242423432');
+INSERT INTO `employees` (`id`, `name`, `email_id`, `phone_number`) VALUES ('2', 'Raj', 'raj@xyz.com', '44234324');
+INSERT INTO `employees` (`id`, `name`, `email_id`, `phone_number`) VALUES ('3', 'Rani', 'rani@xyz.com', '46456546456');
+INSERT INTO `employees` (`id`, `name`, `email_id`, `phone_number`) VALUES ('4', 'Martin', 'martin@xyz.com', '535353545');
